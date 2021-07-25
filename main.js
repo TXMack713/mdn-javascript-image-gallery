7f88f814445d4c5c49a1ffc3b99b80d5ff56364e
Anthony <49073356+TXMack713@users.noreply.github.com>
2021-07-25 08:12:17 -0500

    Add files via upload
    Finished the image gallery project.

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+  let path = "images/pic" + i + ".jpg";
+  newImage.setAttribute("src", path);
+
+  newImage.addEventListener("click", () => {
+    displayedImage.setAttribute("src", path);
+  });
+  thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", () => {
+  let current = btn.getAttribute("class");
+  if (current === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+});
